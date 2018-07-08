e19f528cbf7e27e005fdd297645573e3bba7b6d6
Sebastián Gurin <sebastigurin@gmail.com>
2018-07-08 01:01:34 -0300

    build

--- webpack.prod.js
+++ webpack.prod.js
@@ -7,8 +7,7 @@ module.exports = merge(common, {
   mode: 'production',
   plugins: [
     new HtmlWebpackPlugin({
-      template: 'src/index.html',
-      minify: true
+      template: 'src/index.html'
     })
   ],
 });
